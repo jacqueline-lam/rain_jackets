ed38ec412e7db41c294b794c76f06e0fcb7fda74
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-25 21:42:50 +0000

    Refactor environment

--- lib/rain_jackets.rb
+++ lib/rain_jackets.rb
@@ -9,8 +9,8 @@ require_relative './rain_jackets/cli'
 
 
 ### NAMESPACE MODULE ###
-module RainJackets
-  class Error < StandardError; end
-
-  # Your code goes here...
-end
+# module RainJackets
+#   class Error < StandardError; end
+  
+#   # Your code goes here...
+# end
--- lib/rain_jackets/scraper.rb
+++ lib/rain_jackets/scraper.rb
@@ -3,7 +3,6 @@ class RainJackets::Scraper
   def get_page
     doc = Nokogiri::HTML(open("https://www.outdoorgearlab.com/topics/clothing-womens/best-rain-jacket-womens"))
     puts doc
-    
   end
 
 end
