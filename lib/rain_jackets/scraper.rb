f132eb45bcb69ebb1813f7d6b86a81af1c1e838c
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-28 15:25:51 +0000

    Add method scrape_jackets_table to Scraper

--- lib/rain_jackets/scraper.rb
+++ lib/rain_jackets/scraper.rb
@@ -1,8 +1,11 @@
 class RainJackets::Scraper
 
   def get_page
-    doc = Nokogiri::HTML(open("https://www.outdoorgearlab.com/topics/clothing-womens/best-rain-jacket-womens"))
-    puts doc
+    Nokogiri::HTML(open("https://www.outdoorgearlab.com/topics/clothing-womens/best-rain-jacket-womens"))
   end
-
+  
+  def scrape_jackets_table
+    self.get_page.css("div.content_table_xwide.compare_table tbody")
+  end
+  
 end
