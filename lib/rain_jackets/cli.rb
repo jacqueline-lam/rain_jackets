90cc8207e399b1ac3a2f7f1ba16ebdd107c1cdd6
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-30 18:27:36 +0000

    Create class references to support Jacket.all

--- lib/rain_jackets/cli.rb
+++ lib/rain_jackets/cli.rb
@@ -1,13 +1,12 @@
 class RainJackets::CLI
+  attr_accessor :jackets
+  
+  def initialize
+    self.jackets = RainJackets::Scraper.initialize_jacket_objects(self)
+  end
  
   def call #test
     puts "Welcome to the Best Rain Jackets Rater!"
-  end
-
-  # Uses Scraper class to create new jackets
-  def make_jackets
-    jackets_array = RainJackets::Scraper.initialize_jacket_objects
-    RainJackets::Jacket.create_from_collection(jackets_array)
   end
 
 end
--- lib/rain_jackets/jacket.rb
+++ lib/rain_jackets/jacket.rb
@@ -1,23 +1,25 @@
 class RainJackets::Jacket
   attr_accessor :name, :url, :price, :description, :pros, :cons, :overall_rating, :water_resistance_rating, :breathability_rating, :comfort_rating, :weight_rating, :durability_rating, :packed_size_rating
+  attr_accessor :cli
   
-  @@all = []
+  @@jackets = []
   
   # Take in an argument of an array and sets new jacket's attributes
-  def initialize
-    @@all << self
+  def initialize(cli_ref)
+    self.cli = cli_ref
+    @@jackets << self
   end
 
   # Uses Scraper class to create new jacket
-  def self.create_from_collection(jackets_array)
-   ## Iterate over array of jackets and create a 
-    # new individual jacket using each index
-    jackets_array.each do |jacket_instance|
-      jacket_instance = RainJackets::Jacket.new
-    end
-  end
+  # def self.create_from_collection(jackets_array)
+  # ## Iterate over array of jackets and create a 
+  #   # new individual jacket using each index
+  #   jackets_array.each do |jacket_instance|
+  #     jacket_instance = RainJackets::Jacket.new
+  #   end
+  # end
   
-  def self.all 
-    @all
+  def self.all
+    return @@jackets
   end
 end
