a8a9d5cd878e9a5f80ced271d6be3398252fcddd
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-30 19:39:22 +0000

    Add pseudocode for RainJackets::CLI.call method

--- lib/rain_jackets/cli.rb
+++ lib/rain_jackets/cli.rb
@@ -10,14 +10,29 @@ class RainJackets::CLI
     input = ""
     while input != "exit"
       puts "To see all the best rain jackets, enter 'list all'."
-      puts "To list rain jackets by overall rating, enter 'list jackets by rating'."
+      puts "To list rain jackets by overall rating, enter 'list by ratings'."
       puts "To see all info about a specific jacket, enter jacket by '1-5'."
         # pros
         # cons
         # description 
         # url
         # rating categories 
+    
+    input = gets.chomp
+    if input = "list all"
+      @jackets.each_with_index(1) do |jacket, i)
+        puts "#{i}. #{jacket.name} - Price: #{jacket.price} - Overall Rating: #{jacket.overal_rating}"
+      end
+    #elsif input = 'list by ratings'
+    #elsif input = '1' #jacket 1
+    #elsif input = '2' #jacket 2
+    #elsif input = '3' #jacket 3
+    #elsif input = '4' #jacket 4
+    #elsif input = '5' #jacket 5
+    
     end
+    
+
   end
 
 end
