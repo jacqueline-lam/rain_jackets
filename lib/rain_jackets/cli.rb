a71a4bcd308d24249a6e4ec8c8b20a0015330eb5
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-30 20:21:44 +0000

    Fix minor bugs in RainJackets::CLI

--- lib/rain_jackets/cli.rb
+++ lib/rain_jackets/cli.rb
@@ -18,21 +18,19 @@ class RainJackets::CLI
         # url
         # rating categories 
     
-    input = gets.chomp
-    if input = "list all"
-      @jackets.each_with_index(1) do |jacket, i)
-        puts "#{i}. #{jacket.name} - Price: #{jacket.price} - Overall Rating: #{jacket.overal_rating}"
+      input = gets.chomp
+      if input == "list all"
+        @jackets.each_with_index(1) do |jacket, i|
+          puts "#{i}. #{jacket.name} - Price: #{jacket.price} - Overall Rating: #{jacket.overal_rating}"
+        end
+      #elsif input = 'list by ratings'
+      #elsif input = '1' #jacket 1
+      #elsif input = '2' #jacket 2
+      #elsif input = '3' #jacket 3
+      #elsif input = '4' #jacket 4
+      #elsif input = '5' #jacket 5
+
       end
-    #elsif input = 'list by ratings'
-    #elsif input = '1' #jacket 1
-    #elsif input = '2' #jacket 2
-    #elsif input = '3' #jacket 3
-    #elsif input = '4' #jacket 4
-    #elsif input = '5' #jacket 5
-    
-    end
-    
-
+    end    
   end
-
 end
