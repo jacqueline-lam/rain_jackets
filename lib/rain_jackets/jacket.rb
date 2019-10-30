ea73ff6c8ceca76599cf0eb0e1ce929058e13465
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-30 20:16:57 +0000

    Remove unnecessary instance variable @cli in Jacket class

--- lib/rain_jackets/jacket.rb
+++ lib/rain_jackets/jacket.rb
@@ -1,12 +1,10 @@
 class RainJackets::Jacket
   attr_accessor :name, :url, :price, :description, :pros, :cons, :overall_rating, :water_resistance_rating, :breathability_rating, :comfort_rating, :weight_rating, :durability_rating, :packed_size_rating
-  attr_accessor :cli
   
   @@all = []
   
   # Take in an argument of an array and sets new jacket's attributes
   def initialize(cli_ref)
-    @cli = cli_ref
     @@all << self
   end
 
