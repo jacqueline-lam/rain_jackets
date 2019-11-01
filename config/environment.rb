078aa80b1e1521b874f31bb65b3eeffd8447fc41
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-11-01 21:33:37 +0000

    Create config/environment.rb to load the enviornment

---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+# Load my envrionment - files I need to make my program work
+require_relative '../lib/rain_jackets'
+require_relative '../lib/rain_jackets/cli'
+require_relative '../lib/rain_jackets/scraper'
+require_relative '../lib/rain_jackets/jacket'
+
